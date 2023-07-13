2d1f0f278f4c910a4280a08f2e3cdd23a95187ca
MartinKruess <martin.kruss@digitalcareerinstitute.org>
2023-07-14 01:35:36 +0200

    added git page

---
+++ src/comps/programmingBasic/npm.jsx
@@ -0,0 +1,9 @@
+export const NPM = () => {
+  return (
+    <article>
+      <h2 className="font-extrabold text-3xl my-5">
+        kommen die wichtigsten Infos über GIT in
+      </h2>
+    </article>
+  );
+};
