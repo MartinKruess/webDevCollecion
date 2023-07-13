ed206f72417865cf3c828a31bc5a2621b5fb1de1
MartinKruess <martin.kruss@digitalcareerinstitute.org>
2023-07-13 14:39:11 +0200

    var

--- src/comps/programmingBasic/loop.jsx
+++ src/comps/programmingBasic/loop.jsx
@@ -1,31 +1,44 @@
 export const Loop = () => {
   return (
     <article className="p-4">
-      <h2 className="font-extrabold text-2xl">Die verschiedenen Arten Schleifen</h2>
+      <h2 className="font-extrabold text-2xl">
+        Die verschiedenen Arten Schleifen
+      </h2>
       <p className="pt-4 pb-2">
-        Unter einer Schleife (Loop) versteht man eine abgeschlossene, sich selbst wiederholende Codeabfolge, die solange ausgeführt wird, bis die Bedingung nicht mehr "ture" ist.
+        Unter einer Schleife (Loop) versteht man eine abgeschlossene, sich
+        selbst wiederholende Codeabfolge, die solange ausgeführt wird, bis die
+        Bedingung nicht mehr "ture" ist.
       </p>
       <div className="code">
         <pre>
           <code>
-            for(let i = 0; i &lt; 5; i++) &#123;<br />
-            &#9;console.log("Do something", i+1, "times")<br />
+            for(let i = 0; i &lt; 5; i++) &#123;
+            <br />
+            &#9;console.log("Do something", i+1, "times")
+            <br />
             &#125;
           </code>
         </pre>
       </div>
       <p className="pt-2 pb-4">
-        Wir unterscheiden zwischen drei verschiedenen Loops. Die wohl verbreiteste Schleife ist die "for-loop", wie im obrigen Beispiel. Eine ältere Alternative zur for-loop ist die while loop während die do-while loop sich gänzlich anders verhält.
+        Wir unterscheiden zwischen drei verschiedenen Loops. Die wohl
+        verbreiteste Schleife ist die "for-loop", wie im obrigen Beispiel. Eine
+        ältere Alternative zur for-loop ist die while loop während die do-while
+        loop sich gänzlich anders verhält.
       </p>
       <div className="pt-4 pb-2">
-        <h2 className="font-extrabold text-2xl pt-8">Die verschiedenen Art von Loops:</h2>
+        <h2 className="font-extrabold text-2xl pt-8">
+          Die verschiedenen Art von Loops:
+        </h2>
         <h3 className="font-extrabold text-xl pt-8">for und while</h3>
         <div className="code">
           <div>
             <pre>
               <code>
-                for(let i = 0; i &lt; 5; i++) &#123;<br />
-                &#9;console.log(i, "ist kleiner als 5!")<br />
+                for(let i = 0; i &lt; 5; i++) &#123;
+                <br />
+                &#9;console.log(i, "ist kleiner als 5!")
+                <br />
                 &#125;
               </code>
             </pre>
@@ -33,8 +46,10 @@ export const Loop = () => {
           <div>
             <pre>
               <code>
-                while (let i = 0; i &lt; 5; i++) &#123;<br />
-                &#9;console.log(i, "ist kleiner als 5!")<br />
+                while (let i = 0; i &lt; 5; i++) &#123;
+                <br />
+                &#9;console.log(i, "ist kleiner als 5!")
+                <br />
                 &#125;
               </code>
             </pre>
@@ -42,13 +57,15 @@ export const Loop = () => {
         </div>
         <h3 className="font-extrabold text-xl pt-8">do-while</h3>
         <div className="code">
-          
           <pre>
             <code>
               let i = 0<br />
-              do &#123;<br />
-              &#9;console.log(i, "ist kleiner als 5!")<br />
-              &#125; while (i &lt; 5)<br />
+              do &#123;
+              <br />
+              &#9;console.log(i, "ist kleiner als 5!")
+              <br />
+              &#125; while (i &lt; 5)
+              <br />
             </code>
           </pre>
         </div>
@@ -57,18 +74,29 @@ export const Loop = () => {
       <div className="py-4 code">
         <pre>
           <code>
-            const quote = "Thanks God it´s Friday!"<br />
-            const vowels = "aeiou"<br /><br />
-            for(let q = 0; q &lt; quote.lenght; q++)&#123;<br />
-            &#32;let quoteLetter = quote[q]<br /><br />
-
-            &#32;for(let v = 0; v &lt; vowels.lenght; v++)&#123;<br />
-            &#9;let vowelsLetter = vowels[v]<br /><br />
-
-            &#9;if(quoteLetter === vowelsLetter)&#123;<br />
-            &#9;&#9;console.log(quoteLetter)<br />
-              &#9;&#125;<br />
-              &#32;&#125;<br />
+            const quote = "Thanks God it´s Friday!"
+            <br />
+            const vowels = "aeiou"
+            <br />
+            <br />
+            for(let q = 0; q &lt; quote.lenght; q++)&#123;
+            <br />
+            &#32;let quoteLetter = quote[q]
+            <br />
+            <br />
+            &#32;for(let v = 0; v &lt; vowels.lenght; v++)&#123;
+            <br />
+            &#9;let vowelsLetter = vowels[v]
+            <br />
+            <br />
+            &#9;if(quoteLetter === vowelsLetter)&#123;
+            <br />
+            &#9;&#9;console.log(quoteLetter)
+            <br />
+            &#9;&#125;
+            <br />
+            &#32;&#125;
+            <br />
             &#125;
           </code>
         </pre>
@@ -77,5 +105,5 @@ export const Loop = () => {
         Gelegentlich kommt es vor, dass man einen Nested Loop aufbauen muss.
       </p>
     </article>
-  )
-}
+  );
+};
--- src/comps/programmingBasic/vsCode.jsx
+++ src/comps/programmingBasic/vsCode.jsx
@@ -1,10 +1,6 @@
 import { useEffect, useState } from "react";
 
 export const VSCode = () => {
-  const [isWatched, setIsWatched] = useState(false);
-
-  useEffect(() => {}, [isWatched]);
-
   return (
     <article className="p-4">
       <h2 className="font-extrabold text-2xl">VS Code - Tipps und Tricks</h2>
@@ -27,33 +23,51 @@ export const VSCode = () => {
         </li>
         <li>- alles leicht vom selben Fenster aus zu bedienen</li>
       </ul>
-      <h3 className="font-extrabold text-xl">Git und Github</h3>
+      <h3 className="font-extrabold text-xl">Github</h3>
       <p>
         Um die Arbeit mit Git und Github zu erleichtern gibt es etliche Tools,
         die einem versprechen, dass nun alles besser wird. Manche von ihnen sind
-        Kostenpflichtig, andere Kostenlos. Doch was brauchen wir wirklich und
+        kostenpflichtig, andere kostenlos. Doch was brauchen wir wirklich und
         was können wir sogar innerhalb von VS-Code nutzen? VS-Code bietet durch
         Extensions die Möglichkeit uns Tools direkt in die Entwicklungsumgebung
         zu holen.
       </p>
-
+      <p>
+        Die "GitHub Pull request and Issues" Extension ist eine erweiterung, die
+        das Arbeiten mit Github direkt in VS-Code möglich macht. Über das Github
+        Symbol am linken Rand kannst du nun deinen Github Account mit deinem
+        VS-Code verknüpfen.
+      </p>
       <div className="imgContainer">
-        {isWatched ? (
-          <img
-            className="imgHover"
-            onClick={() => setIsWatched(!isWatched)}
-            src="./images/ghExtension.png"
-            alt="Github Extension big"
-          />
-        ) : (
-          <img
-            className="imgNormal"
-            onClick={() => setIsWatched(!isWatched)}
-            src="./images/ghExtension.png"
-            alt="Github Extension preview"
-          />
-        )}
+        <img
+          className="imgNormal small"
+          src="./images/extension-signin.png"
+          alt="Github Extension preview"
+        />
       </div>
+      <p>
+        Nach dem Einloggen und Authorisieren von Github für VS-Code kannst du
+        über diese Extension bequem git add, commit, push und pull verwenden.
+        Ebenfalls lassen sich dadurch mit zwei Klicks neue Repos auf Github
+        erstellen.
+      </p>
+      <p>
+        Ergänzend dazu könnte man noch folgende Extension nutzen. Genaues über
+        diese Extension weiß ich leider aktuell nicht.
+      </p>
+      <div className="imgContainer">
+        <img
+          className="imgNormal"
+          src="./images/ghExtension.png"
+          alt="Github Extension preview"
+        />
+      </div>
+      <h3 className="font-extrabold text-xl">HTML Tags</h3>
+      <h3 className="font-extrabold text-xl">
+        Curly Brackets/Geschweifte Klammern
+      </h3>
+      <h3 className="font-extrabold text-xl">Github</h3>
+
       <div className="code">
         <pre>
           <code>
