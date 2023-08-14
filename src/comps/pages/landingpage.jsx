621b13767bde4618137596edda3c39343116b7c3
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2023-08-14 13:04:53 +0200

    landingpage

--- src/comps/pages/landingpage.jsx
+++ src/comps/pages/landingpage.jsx
@@ -3,14 +3,29 @@ export const Landingpage = () => {
     <section className="w-10/12 m-auto">
       <article className="mt-10 p-4">
         <h1>
-          Willkommen an alle Lernenden. 
+          Fullstack WebDev
         </h1>
         <p>
-          Hier entsteht eine Dokumentation über die jewieligen Teilthemen der einzelnen Module. Ziel des Ganzen ist es eine Plattform zu schaffen auf der ihr Informationen leicht verständlich nachlesen könnt, externe Übungsaufgaben findet und eine Linksammlung zu diversen Themen zur Verfügung steht.
+          Willkommen an alle Lernenden.
+          Hier entsteht eine Dokumentation über die jewieligen Teilthemen der einzelnen Module. Ziel des Ganzen ist es eine Plattform zu schaffen auf der ihr Informationen leicht verständlich nachlesen könnt, zusätzliche Übungsaufgaben findet und euch eine Linksammlung zu diversen Themen zur Verfügung steht.
         </p>
         <p>
-          Die Seite befindet sich noch in den Kinderschuhen und nur der JS Part hat ein paar wenige Einträge.
+          Die Seite befindet sich noch im Aufbau, daher erhaltet ihr hier eine kleine Übersicht über den aktuellen Stand der Webseite.
         </p>
+        <ul className="flex flex-wrap w-80">
+          <li className="w-1/2">HTML & CSS</li>
+          <li className="w-1/2">90%</li>
+          <li className="w-1/2">Javascript inkl. TS</li>
+          <li className="w-1/2">65%</li>
+          <li className="w-1/2">React</li>
+          <li className="w-1/2">0%</li>
+          <li className="w-1/2">Node Express</li>
+          <li className="w-1/2">0%</li>
+          <li className="w-1/2">Database</li>
+          <li className="w-1/2">0%</li>
+          <li className="w-1/2">Deployment</li>
+          <li className="w-1/2">0%</li>
+        </ul>
       </article>
     </section>
   )
