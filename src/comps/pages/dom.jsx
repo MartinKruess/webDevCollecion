4809e29807a0b4e5e0e5d19c398e24cf690daa11
MartinKruess <martin.kruss@digitalcareerinstitute.org>
2023-09-13 09:36:55 +0200

    dom text

--- src/comps/pages/dom.jsx
+++ src/comps/pages/dom.jsx
@@ -1,21 +1,13 @@
 import { useContext } from "react";
 import { ThemeContext } from "../global/loadTheme";
 
-import { NPM } from "../programmingBasic/npm";
-import { Variable } from "../programmingBasic/variable";
-import { IfElse } from "../programmingBasic/ifelse";
-import { Array } from "../programmingBasic/array";
-import { Ternary } from "../programmingBasic/ternary";
-import { JSText } from "../programmingBasic/jsText";
-import { Loop } from "../programmingBasic/loop";
-import { NodeInstaller } from "../programmingBasic/node";
-import { Function } from "../programmingBasic/function";
-import { JSExercises } from "../programmingBasic/exercisesAL";
+import { DOMText } from "../dom/domText";
+import { Querries } from "../dom/querys";
 
+import { NPM } from "../programmingBasic/npm";
+import { JSExercises } from "../programmingBasic/exercisesAL";
 import { Vite } from "../programmingBasic/vite";
-import { Objects } from "../programmingBasic/objects";
-import { ObjectsAdv } from "../programmingBasic/objectsAdv";
-import { Datentypen } from "../programmingBasic/datatype";
+
 import { SelfTest } from "../programmingBasic/jsSelfText";
 import { Electron } from "../programmingBasic/electron";
 
@@ -24,16 +16,16 @@ export const DOM = () => {
 
   return (
     <>
-      <nav className="bg-slate-800 pb-5 px-8 sticky z-10 top-24 sm:top-16">
-        <ul className="w-full text-sm sm:text-lg md:text-xl flex flex-wrap gap-2 sm:gap-3 pt-2 m-auto">
+      <nav className='bg-slate-800 pb-5 px-8 sticky z-10 top-24 sm:top-16'>
+        <ul className='w-full text-sm sm:text-lg md:text-xl flex flex-wrap gap-2 sm:gap-3 pt-2 m-auto'>
           <li
-            className="p-1 sm:p-2 border rounded-lg bg-green-950 hover:bg-red-800"
+            className='p-1 sm:p-2 border rounded-lg bg-green-950 hover:bg-red-800'
             onClick={() => setTheme("node")}
           >
             Node
           </li>
           <li
-            className="p-1 sm:p-2 border rounded-lg bg-green-950 hover:bg-red-800"
+            className='p-1 sm:p-2 border rounded-lg bg-green-950 hover:bg-red-800'
             onClick={() => setTheme("npm")}
           >
             NPM
@@ -51,73 +43,37 @@ export const DOM = () => {
             Electron
           </li> */}
           <li
-            className="p-1 sm:p-2 border rounded-lg bg-green-950 hover:bg-red-800"
+            className='p-1 sm:p-2 border rounded-lg bg-green-950 hover:bg-red-800'
             onClick={() => setTheme("objectsAdv")}
           >
             JSON
           </li>
           <li
-            className="p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800"
-            onClick={() => setTheme("variables")}
+            className='p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800'
+            onClick={() => setTheme("querry")}
           >
-            Variables
+            Querry
           </li>
           <li
-            className="p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800"
-            onClick={() => setTheme("datatype")}
-          >
-            Datentypen
-          </li>
-          <li
-            className="p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800"
-            onClick={() => setTheme("ifelse")}
-          >
-            If/Else
-          </li>
-          <li
-            className="p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800"
-            onClick={() => setTheme("array")}
-          >
-            Arrays
-          </li>
-          <li
-            className="p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800"
-            onClick={() => setTheme("loop")}
-          >
-            Loops
-          </li>
-          <li
-            className="p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800"
-            onClick={() => setTheme("function")}
-          >
-            Functions
-          </li>
-          <li
-            className="p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800"
-            onClick={() => setTheme("objects")}
-          >
-            Objects 
-          </li>
-          <li
-            className="p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800"
+            className='p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800'
             onClick={() => setTheme("objectsAdv")}
           >
-            Objects <span className="text-sm">(Adv)</span>
+            Objects <span className='text-sm'>(Adv)</span>
           </li>
           <li
-            className="p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800"
+            className='p-1 sm:p-2 border rounded-lg bg-blue-950 hover:bg-red-800'
             onClick={() => setTheme("classes")}
           >
             Classes
           </li>
           <li
-            className="p-1 sm:p-2 border rounded-lg bg-red-950 hover:bg-red-800"
+            className='p-1 sm:p-2 border rounded-lg bg-red-950 hover:bg-red-800'
             onClick={() => setTheme("jsexercises")}
           >
             Übungen
           </li>
           <li
-            className="p-1 sm:p-2 border rounded-lg bg-red-950 hover:bg-red-800"
+            className='p-1 sm:p-2 border rounded-lg bg-red-950 hover:bg-red-800'
             onClick={() => setTheme("jsSelfTest")}
           >
             Selbst Test
@@ -130,14 +86,14 @@ export const DOM = () => {
           </li> */}
         </ul>
       </nav>
-      <section className="w-10/12 m-auto">
-        {!theme && <JSText />}
+      <section className='w-10/12 m-auto'>
+        {!theme && <DOMText />}
         {theme === "npm" && <NPM />}
         {theme === "node" && <NodeInstaller />}
         {theme === "vs-code" && <VSCode />}
         {theme === "vite" && <Vite />}
         {theme === "electron" && <Electron />}
-        {theme === "console" && <Console />}
+        {theme === "querry" && <Querries />}
         {theme === "variables" && <Variable />}
         {theme === "ifelse" && <IfElse />}
         {theme === "array" && <Array />}
