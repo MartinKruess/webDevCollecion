c5fb5b0bd7ae1d34cfe4521ba5aa058a43b8760d
MartinKruess <martin.kruss@digitalcareerinstitute.org>
2024-06-13 10:27:44 +0200

    add js exercise

--- src/comps/programmingBasic/exercisesAL.jsx
+++ src/comps/programmingBasic/exercisesAL.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import { jsExercises } from "./assets/exercises";
+import { useEffect, useState } from 'react';
+import { jsExercises } from './assets/exercises';
 
 export const JSExercises = () => {
-  const [type, setType] = useState("default");
+  const [type, setType] = useState('default');
   const [jsExercisesArr, setJSExercisesArr] = useState([]);
 
   const getTypeTarget = (e) => {
@@ -12,45 +12,46 @@ export const JSExercises = () => {
   useEffect(() => {
     const filteredExercises =
       jsExercises && jsExercises.filter((execise) => execise.type === type);
-    type === "default"
+    type === 'default'
       ? setJSExercisesArr(jsExercises)
       : setJSExercisesArr(filteredExercises);
   }, [type]);
 
   return (
-    <article className="p-4">
-      <h2 className="font-extrabold text-2xl">Bonus Übungen</h2>
-      <p className="pt-4 pb-2">
+    <article className='p-4'>
+      <h2 className='font-extrabold text-2xl'>Bonus Übungen</h2>
+      <p className='pt-4 pb-2'>
         Hier sind ein paar zusätzliche Übungsaufgaben. Erstelle einfach eine
         index.js/main.js und arbeite so viele Aufgaben durch, wie du magst. Ich
         nehme gerne Spiele als Beispiele, da in der Spiele Entwicklung sehr
         viele Sinnvolle Loops und Functions angewendet werden.
       </p>
-      <div className="md:flex justify-between mt-10">
+      <div className='md:flex justify-between mt-10'>
         <select
-          className="bg-slate-700 px-6 py-2 rounded-xl h-10 w-full md:flex md:justify-between md:w-1/6 font-bold"
-          name="exercises"
-          id="exercises"
+          className='bg-slate-700 px-6 py-2 rounded-xl h-10 w-full md:flex md:justify-between md:w-1/6 font-bold'
+          name='exercises'
+          id='exercises'
           onChange={(e) => getTypeTarget(e)}
         >
-          <option value="default">Alle</option>
-          <option value="array">Array Basic</option>
-          <option value="loops">Loop Basic</option>
-          <option value="objects">Object</option>
-          <option value="this" disabled>
+          <option value='default'>Alle</option>
+          <option value='math'>Math</option>
+          <option value='array'>Array Basic</option>
+          <option value='loops'>Loop Basic</option>
+          <option value='objects'>Object</option>
+          <option value='this' disabled>
             this
           </option>
-          <option value="arrayHigh" disabled>
+          <option value='arrayHigh' disabled>
             Map, Filter, Reduce, Find
           </option>
         </select>
-        <p className="text-lg py-2 w-full text-right md:w-1/6">
+        <p className='text-lg py-2 w-full text-right md:w-1/6'>
           Übungen: {jsExercisesArr.length}
         </p>
       </div>
       {jsExercisesArr.map((exercise, i) => (
-        <div className="mt-5 p-5 bg-slate-800 border" key={i}>
-          <h3 className="font-extrabold text-xl">{exercise.header}</h3>
+        <div className='mt-5 p-5 bg-slate-800 border' key={i}>
+          <h3 className='font-extrabold text-xl'>{exercise.header}</h3>
           <p>{exercise.tasks[0]}</p>
           <p>{exercise.tasks[1]}</p>
           {exercise.code && (
@@ -62,13 +63,13 @@ export const JSExercises = () => {
               <code>{exercise.code[2]}</code>
             </pre>
           )}
-          <h3 className="font-extrabold text-xl">{exercise.advancedHeader}</h3>
+          <h3 className='font-extrabold text-xl'>{exercise.advancedHeader}</h3>
           <p>{exercise.advancedTask}</p>
           {exercise.output && (
             <a
               href={exercise.output}
               download={exercise.output}
-              target="_blank"
+              target='_blank'
             >
               Download
             </a>
--- src/comps/ui-basics/assets/animationPart4.jsx
+++ src/comps/ui-basics/assets/animationPart4.jsx
@@ -2,11 +2,7 @@ export const AnimationPart4 = () => {
   return (
     <article className='text-sm min-h-full border' id='ani4'>
       <h3 className='text-xl my-2 pt-2'>Was sind Keyframes?</h3>
-<<<<<<< HEAD
-      <p class='p-0'>
-=======
       <p className='p-0'>
->>>>>>> 4a3c4b7d29aaa5ec655e6477c108496c4ed16646
         Ein Keyframe kann man sich als Einzelabschnitt einer Animation
         vorstellen in dem ein Gewisser Zustand definiert wird. In CSS
         Animationen werden Keyframes verwendet, um die Zwischenzustände einer
@@ -14,22 +10,13 @@ export const AnimationPart4 = () => {
         CSS-Regeln, die den Zustand eines Elements zu einem bestimmten Zeitpunkt
         in der Animation beschreiben.
       </p>
-<<<<<<< HEAD
-      <p class='p-0'>
-=======
       <p className='p-0'>
->>>>>>> 4a3c4b7d29aaa5ec655e6477c108496c4ed16646
         Hier ist ein Beispiel: Der Slider bewegt sich anfangs schnell, dann
         langsam ans Ende und auf dem Rückweg mit mittlerer, gleichbleibender
         Geschwindigkeit.
       </p>
-<<<<<<< HEAD
-      <div class='border'>
-        <div class='slider'></div>
-=======
       <div className='border'>
         <div className='slider'></div>
->>>>>>> 4a3c4b7d29aaa5ec655e6477c108496c4ed16646
       </div>
     </article>
   );
